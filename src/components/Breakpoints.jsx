5553215ffccfc5c9d6224fadf4a80c112fefbe2d
Ole Martin Kristiansen <olemak@apasje.no>
2017-11-27 00:16:53 +0100

    Placeholder components for Breakpoints and Spacing

---
+++ src/components/Breakpoints.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Breakpoints extends Component {
+    render() {
+        return (
+            <div className="Breakpoints">
+                <h3 className="Breakpoints__header">Breakpoints</h3>
+            </div>
+        );
+    }
+}
+
+export default Breakpoints;
---
+++ src/components/Spacing.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Spacing extends Component {
+    render() {
+        return (
+            <div className="Spacing">
+                <h3 className="Spacing__header">Spacing</h3>
+            </div>
+        );
+    }
+}
+
+export default Spacing;
