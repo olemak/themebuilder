df5ac664795ea9ca1fae729019c923795da4976a
Ole Martin Kristiansen <olemak@apasje.no>
2017-11-27 23:28:38 +0100

    Font selection - start

--- src/components/Fonts.jsx
+++ src/components/Fonts.jsx
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
 class Fonts extends Component {
+    state = {
+        selectedFonts: [],
+        googleFontList: [
+            {family: ""}
+        ]
+    }
+    
+    componentDidMount = () => {
+        let GoogleFontsRequest = new Request ("https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyAu6iNE9RBHkTp9eEPKk61ua0meOwb--Ps")
+        fetch(GoogleFontsRequest)
+        .then(response=>response.json())
+        .then( data =>{
+            this.setState({ googleFontList: data.items })
+        })
+    }
+    
+    fontSelect = ( event ) => {
+        if (this.state.googleFontList.find(font=>font.family === event.target.value)) {
+            console.log(event.target.value);
+            //DO THINGS ON FONT SELECTION HERE
+        }
+    }
+
     render() {
         return (
             <div className="Fonts">
                 <h3 className="fonts__header">Fonts</h3>
+                <input list="googleFonts" onInput={this.fontSelect} name="font" />
+                <datalist id="googleFonts" >
+                  {this.state.googleFontList.map(font=><option value={font.family} key={font.family}/>)}
+                </datalist>
             </div>
         );
     }
--- src/components/Typography.jsx
+++ src/components/Typography.jsx
@@ -4,7 +4,13 @@ class Typography extends Component {
     render() {
         return (
             <div className="Typography">
-                <h3 className="Typography__header">Typography</h3>
+                <h3 className="typography__header">Typography</h3>
+
+                <h3 className="typography__headers">Headers</h3>
+                <h3>H1</h3><h1>The fearsome Tarrasque</h1>
+                <h3 className="typography__body">Body</h3>
+                <p className="typography__paragraph">Paragraph</p>
+                <small className="typography__paragraph">Small</small>
             </div>
         );
     }
