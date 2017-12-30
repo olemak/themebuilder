b7bf03adcbdc9a1b065e7602d0f535c8e468b895
Ole Martin Kristiansen <olemak@apasje.no>
2017-12-30 15:00:57 +0100

    Download theme variables as json file

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import ReactJson from 'react-json-view';
 
 class Dashboard extends Component {
+
+    downloadJSON(themevariables){
+        let element = document.createElement('a');
+            element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(themevariables)));
+            element.setAttribute('download', "theme.json");          
+            element.style.display = 'none';
+            document.body.appendChild(element);
+            element.click();
+          
+            document.body.removeChild(element);
+    }
+
     render() {
         return (
             <div className="Dashboard">
@@ -9,12 +21,13 @@ class Dashboard extends Component {
 
                 <h5>Current theme variables</h5>
                 <ReactJson src={this.props.state} collapsed={true}/>
+                
+                <br />
 
-                <button disabled>Download JSON<br />Download theme variables in JSON file format</button>
+                <button onClick={()=>{this.downloadJSON(this.props.state)}}>Download JSON<br />Download theme variables in JSON file format</button>
                 <button disabled>Save<br />Save to theme.json file in project root directory.</button>
                 <button disabled>Remember<br />Store current vaiables to localstorage</button>
                 
-
             </div>
         );
     }
