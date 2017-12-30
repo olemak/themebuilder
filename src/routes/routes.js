015f3463fc0bb705dbaf03c988c5fadec4019ccd
Ole Martin Kristiansen <olemak@apasje.no>
2017-12-30 12:46:38 +0100

    commented out typography

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,11 @@
-//import Aspect from '../components/Aspect.jsx';
-//import Palette from '../components/Palette.jsx';
-//import Breakpoints from '../components/Breakpoints.jsx';
 import BreakpointContainer from '../components/BreakpointContainer.js';
-import Dashboard from '../components/Dashboard.jsx';
-//import Fonts from '../components/Fonts.jsx';
-//import Spacing from '../components/Spacing.jsx';
 import SpacingContainer from '../components/SpacingContainer.js';
-import Typography from '../components/Typography.jsx';
-
 import ColorContainer from '../components/ColorContainer.js'
 import AspectContainer from '../components/AspectContainer.js'
 import FontContainer from '../components/FontContainer.js'
+
+import Dashboard from '../components/Dashboard.jsx';
+//import Typography from '../components/Typography.jsx';
 
 const routes = () => {
     const components = { 
@@ -20,7 +15,7 @@ const routes = () => {
         Aspect: AspectContainer, 
         Breakpoints: BreakpointContainer, 
         Fonts: FontContainer, 
-        Typography 
+//        Typography 
     };
 
     return Object.keys( components ).map( component => {
