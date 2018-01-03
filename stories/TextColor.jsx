3a2efb7fa84613cceda412f971da9aa0564cdfaf
Ole Martin Kristiansen <olemak@apasje.no>
2018-01-04 00:38:18 +0100

    Text__color, text--backgroundColor

--- stories/TextColor.jsx
+++ stories/TextColor.jsx
@@ -7,15 +7,33 @@ class TextColor extends Component {
     themeTextColors() {
         let textColors = [];
         for (let color in theme.colors) {
-            textColors.push(<section className="text" key={`text-color--${color}`}>
-            <p className={`text text-color--${color}`}>
-                {`.text .text-color--${color}`}<br />
-                {`${color} color on neutral background`}
+            let colorClasses = `text-color__${color}`;
+            let backgroundColorClasses = `text-background--${color}`;
+            let variants = Object.keys(theme.colors[color]);
+            textColors.push(<section className="text story--text" key={`text-color--${color}`}>
+            <p>
+                { colorClasses }<br />
+                <span className={colorClasses}>
+                    {`${color} color on neutral background`}
+                </span>
+
+                {variants.map(variant => <div>
+                    <span className="story--classname">
+                        { colorClasses + "--" + variant }
+                    </span>
+                    <span className={ colorClasses + '--' + variant }>
+                        {`${color} ${variant}`}
+                    </span>
+                    </div>
+                )}
+
             </p>
 
-            <p className={`text text--${color} text__background--contrast`}>
-                {`.text .text--${color} .text__background--contrast`}<br />
-                {`${color} color on contrast background`}
+            <p>
+                { backgroundColorClasses }<br />
+                <span className={ backgroundColorClasses }>
+                    {`${color} color on contrast background`}
+                </span>
             </p>
         </section>)
         }
