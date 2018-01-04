5a1711fc746a70b41ff7e93eeb5f13938608a7cf
Ole Martin Kristiansen <olemak@apasje.no>
2018-01-05 00:56:48 +0100

    regular colors and backgroundcolors

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -7,12 +7,16 @@ import { linkTo } from '@storybook/addon-links';
 import { Button, Welcome } from '@storybook/react/demo';
 
 import TextColor from './TextColor';
+import ThemeColors from './ThemeColors';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+storiesOf('Color', module)
+  .add('Palette', () => <ThemeColors />)
 
 storiesOf('Typography', module)
   .add('Basic Text', () => <div className="basic">Basic Typography</div>)
