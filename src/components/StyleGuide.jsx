cb30c0e4a16bc5bac4319a2566e42975bcf5657d
Ole Martin Kristiansen <olemak@apasje.no>
2018-01-23 22:29:09 +0100

    Styleguide component and scss

---
+++ src/components/StyleGuide.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import './StyleGuide.scss';
+
+class StyleGuide extends Component {
+
+    styleGuide(colors){
+        let palette = [];
+        for (let color in colors) {
+            palette.push(<div className="styleguide" >
+                <h4>{color}</h4>
+                <h5>Value: <div className="styleguide__color" style={{backgroundColor: colors[color].value, borderColor: colors[color].contrast}}/></h5>
+            </div>)
+        }
+        return palette;
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Style Guide</h2>
+                <p>Change things in the tabs (color, fonts etc. to update).</p>
+                <h3>Color</h3>
+                <section className="grid">
+                    {this.styleGuide(this.props.colors)}
+                </section>
+
+            </div>
+        );
+    }
+}
+
+export default StyleGuide;
