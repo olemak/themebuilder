59495d26fce5721f556dc6fe1bc73dd6343eb40b
Ole Martin Kristiansen <olemak@apasje.no>
2018-02-19 15:16:35 +0100

    Typography presets available and toggleable

--- src/components/Typography.jsx
+++ src/components/Typography.jsx
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import presetInfo from '../data/presetInfo.json';
+
+const presetDefaults = {};
+presetInfo.map(preset=>{Object.assign(presetDefaults, {[preset.id]: false })})
+
 
 class Typography extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {presetDefaults};
+    }
+
+    toggleShow = event => {
+        this.setState({[event.target.id]: !this.state[event.target.id]})
+    }
+
     render() {
+        console.log(presetInfo);
         return (
             <div className="Typography">
-                <h3 className="typography__header">Typography</h3>
-
-                <h3 className="typography__headers">Headers</h3>
-                <h3>H1</h3><h1>The fearsome Tarrasque</h1>
-                <h3 className="typography__body">Body</h3>
-                <p className="typography__paragraph">Paragraph</p>
-                <small className="typography__paragraph">Small</small>
+                {presetInfo.map(preset=><section key={preset.id}>
+                    <div>
+                        <span className={`preset__sample preset__sample--${preset.id}`}>{preset.name}</span>
+                    </div>
+                    <input type="checkbox" onChange={this.toggleShow} id={preset.id} />
+                    <label htmlFor={preset.id}>show details</label>
+                    {this.state[preset.id] ? 
+                    <div>
+                        <em className="preset__intro">{preset.intro}</em> 
+                        <p className="preset__description">{preset.description}</p>
+                    </div> : ""}
+                </section>
+                )}
             </div>
         );
     }
