e3bdf4d722c18af84e52c05f4daac3cc2ae20721
Ole Martin Kristiansen <olemak@apasje.no>
2018-02-26 22:32:10 +0100

    Colors values and layout adjusted

--- src/data/defaultColors.js
+++ src/data/defaultColors.js
@@ -1,31 +1,31 @@
 const defaultColors = {
     brand: {
         value: "#057578",
-        contrast: "#ffffff",
+        contrast: "#fafafa",
         light: "#069296",
         dark: "#03575a"
     },
     main: {
         value: "#2c5275",
-        contrast: "#ffffff",
+        contrast: "#fafafa",
         light: "#376692",
         dark: "#213d57"
     },
     support: {
-        value: "#3f578b",
-        contrast: "#ffffff",
-        light: "#4e6cad",
-        dark: "#2f4168"
+        value: "#a720c2",
+        contrast: "#fafafa",
+        light: "#d028f2",
+        dark: "#7d1891"
     },
     black: {
         value: "#333333",
-        contrast: "#ffffff",
+        contrast: "#fafafa",
         light: "#3f3f3f",
         dark: "#262626"
     },
     white: {
         value: "#fafafa",
-        contrast: "#ffffff",
+        contrast: "#333333",
         light: "#ffffff",
         dark: "#b4b4b4"
     },
@@ -36,28 +36,28 @@ const defaultColors = {
         dark: "#8c8c8c"
     },
     info: {
-        value: "#5d94d5",
-        contrast: "#ffffff",
-        light: "#74b9ff",
-        dark: "#456f9f"
+        value: "#74b9ff",
+        contrast: "#333333",
+        light: "#91e7ff",
+        dark: "#578abf"
     },
     success: {
         value: "#61bd7f",
-        contrast: "#ffffff",
+        contrast: "#333333",
         light: "#79ec9e",
         dark: "#488d5f"
     },
     alert: {
-        value: "#b2004e",
-        contrast: "#ffffff",
-        light: "#de0061",
-        dark: "#85003a"
+        value: "#fe0654",
+        contrast: "#000000",
+        light: "#ff0769",
+        dark: "#be043f"
     },
     warn: {
-        value: "#9e1436",
-        contrast: "#ffffff",
-        light: "#c51943",
-        dark: "#760f28"
+        value: "#fc9d01",
+        contrast: "#333333",
+        light: "#ffc401",
+        dark: "#bd7500"
     }
 };
 
