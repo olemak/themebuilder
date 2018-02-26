11b3997507189fce8db7d6deeb0c06ee8f4964e3
Ole Martin Kristiansen <olemak@apasje.no>
2018-02-26 18:31:58 +0100

    New default colors

--- src/data/defaultColors.js
+++ src/data/defaultColors.js
@@ -1,34 +1,63 @@
 const defaultColors = {
     brand: {
-        value: "#123456",
-        contrast: "#ff00ff"
+        value: "#057578",
+        contrast: "#ffffff",
+        light: "#069296",
+        dark: "#03575a"
     },
     main: {
-        value: "#345612"
+        value: "#2c5275",
+        contrast: "#ffffff",
+        light: "#376692",
+        dark: "#213d57"
     },
     support: {
-        value: "#561234"
+        value: "#3f578b",
+        contrast: "#ffffff",
+        light: "#4e6cad",
+        dark: "#2f4168"
     },
     black: {
-        value: "#3b3b3b"
+        value: "#333333",
+        contrast: "#ffffff",
+        light: "#3f3f3f",
+        dark: "#262626"
     },
     white: {
-        value: "#ebebeb"
+        value: "#fafafa",
+        contrast: "#ffffff",
+        light: "#ffffff",
+        dark: "#b4b4b4"
     },
     gray: {
-        value: "#c0c0c0"
+        value: "#bbbbbb",
+        contrast: "#444444",
+        light: "#e9e9e9",
+        dark: "#8c8c8c"
     },
     info: {
-        value: "#0080ff"
+        value: "#5d94d5",
+        contrast: "#ffffff",
+        light: "#74b9ff",
+        dark: "#456f9f"
     },
     success: {
-        value: "#00ff40"
+        value: "#61bd7f",
+        contrast: "#ffffff",
+        light: "#79ec9e",
+        dark: "#488d5f"
     },
     alert: {
-        value: "#ff0000"
+        value: "#b2004e",
+        contrast: "#ffffff",
+        light: "#de0061",
+        dark: "#85003a"
     },
     warn: {
-        value: "#ff702b"
+        value: "#9e1436",
+        contrast: "#ffffff",
+        light: "#c51943",
+        dark: "#760f28"
     }
 };
 
